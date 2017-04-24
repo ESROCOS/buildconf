23521004d0569f2ae25228ce92eda81541b42956
Benjamin Kisliuk <benjamin.kisliuk@dfki.de>
2017-04-24 15:52:32 +0200

    added gitlab support, refactored package-set link

--- init.rb
+++ init.rb
@@ -26,5 +26,6 @@
 require 'autoproj/gitorious'
 Autoproj.gitorious_server_configuration('GITORIOUS', 'gitorious.org')
 Autoproj.gitorious_server_configuration('GITHUB', 'github.com', :http_url => 'https://github.com')
+Autoproj.gitorious_server_configuration('GITLAB', 'git.hb.dfki.de', :http_url => 'https://git.hb.dfki.de.com')
 
 Autoproj.env_inherit 'CMAKE_PREFIX_PATH'
