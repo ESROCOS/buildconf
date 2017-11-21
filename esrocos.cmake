b8ef0f2a4c1d6ec84bd72074cf82cca626e7ecaa
Benjamin Kisliuk <benjamin.kisliuk@dfki.de>
2017-11-21 15:44:48 +0100

    fixed error in cmake function causing to wrongly set linkings list

--- esrocos.cmake
+++ esrocos.cmake
@@ -1,37 +1,46 @@
   INCLUDE(FindPkgConfig)
   set(ENV{PKG_CONFIG_PATH} "${CMAKE_INSTALL_PREFIX}/lib/pkgconfig/")
   set(WRITE_OUT "libs:")
-
+  
 function(esrocos_add_dependency REQ_MODULE)
- 
+  
   pkg_check_modules(LINK_LIBS REQUIRED ${REQ_MODULE})
 
+  set(LOCAL_WO "")
+
   foreach(LIB ${LINK_LIBS_STATIC_LIBRARIES})
-  
+   
     set(NOT_INCLUDED TRUE)
     foreach(DIR ${LINK_LIBS_STATIC_LIBRARY_DIRS})
       if(EXISTS "${DIR}/lib${LIB}.a") 
-        set(WRITE_OUT "${WRITE_OUT}\n- ${DIR}/lib${LIB}.a")
+        set(LOCAL_WO "${LOCAL_WO}\n- ${DIR}/lib${LIB}.a")
         set(NOT_INCLUDED FALSE)
       elseif(EXISTS "${DIR}/lib${LIB}.so") 
-        set(WRITE_OUT "${WRITE_OUT}\n- ${DIR}/lib${LIB}.so")
+        set(LOCAL_WO "${LOCAL_WO}\n- ${DIR}/lib${LIB}.so")
         set(NOT_INCLUDED FALSE)
       endif()
+
+      
     endforeach(DIR)
 
     if(${NOT_INCLUDED})
       find_library(FOUND ${LIB})
       if(EXISTS ${FOUND})
-        set(WRITE_OUT "${WRITE_OUT}\n- ${FOUND}")
+        set(LOCAL_WO "${LOCAL_WO}\n- ${FOUND}" )
       endif()
       unset (FOUND CACHE)
     endif()
 
   endforeach(LIB)
 
+  set(WRITE_OUT "${WRITE_OUT}\n${LOCAL_WO}" PARENT_SCOPE)
+
 endfunction(esrocos_add_dependency)
 
+
 function(esrocos_install_dependency_info)
+
+  message(${WRITE_OUT})
 
   file(WRITE ${CMAKE_BINARY_DIR}/linkings.yml ${WRITE_OUT})
 
